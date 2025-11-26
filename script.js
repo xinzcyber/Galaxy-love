2d25dec3030caa92824ecd07bf21f34d8db0bcbb
Xinz <megaardiansah0@gmail.com>
2025-11-26 23:09:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -588,9 +588,9 @@ scene.add(planet);
 //Ganti Deskripsi Planet
 const ringTexts = [
   'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
+  "I love you havi",//untuk deskripsi planet layer 2
   "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  "22/10/2025",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
